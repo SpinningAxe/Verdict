29cabbfaa5891afe10b813931bd4ee9f258a7824
Vo Van Viet <vovanviet4444@gmail.com>
2023-10-23 04:00:47 +0700

    transition between landingpage and menu

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -9,7 +9,21 @@ import { Router } from '@angular/router';
 export class LandingPageComponent {
   constructor(private route: Router) { }
 
-  navigateTo(){
+  gotomenu(){
+    
     this.route.navigate(['menu-page'])
+  }
+
+  async navigateTo(){
+    let element = document.getElementById("title");
+    element!.classList.remove("appear-animation");
+    element!.classList.add("diaspear-animation");
+    
+    let elementSub = document.getElementById("subtitle_box");
+    elementSub!.classList.add("lower-animation");
+
+    setTimeout(() =>{
+      this.route.navigate(['menu-page'])
+    }, 500) 
   }
 }
