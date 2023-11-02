dc5e0b0eb2b523aa97c7cf3c703ca445a9d86143
Vo Van Viet <vovanviet4444@gmail.com>
2023-11-02 14:14:25 +0700

    assemble checkbox graphics

--- src/app/pages/menu-page/menu-page.component.ts
+++ src/app/pages/menu-page/menu-page.component.ts
@@ -21,15 +21,43 @@ export class MenuPageComponent {
     }, 200) 
   }
 
+  openModal(){
+    let modal = document.getElementById('checkbox');
+    let right_conttent = document.getElementById('right-content');
+    let horizonLine = document.getElementById('horizonLine');
+    
+    right_conttent!.classList.remove("remove-blur-content")
+    right_conttent!.classList.add("blur-content")
+    
+    horizonLine!.classList.remove("remove-blur-content")
+    horizonLine!.classList.add("blur-content")
+
+    modal!.style.display = 'flex';
+  }
+
+  closeModal(){
+    let modal = document.getElementById('checkbox');
+    let right_conttent = document.getElementById('right-content');
+    let horizonLine = document.getElementById('horizonLine');
+
+    right_conttent!.classList.remove("blur-content")
+    right_conttent!.classList.add("remove-blur-content")
+
+    horizonLine!.classList.remove("blur-content")
+    horizonLine!.classList.add("remove-blur-content")
+    
+    modal!.style.display = 'none';
+  }
+
   navigateToVerdict(){
     setTimeout(() =>{
-      this.Router.navigate(['verdict'])
+      // this.Router.navigate(['verdict'])
     }, 200) 
 
   }
 
-  hoverEffect(button:string){
-    let buttonHover = document.getElementById(`${button}`);
-    buttonHover!.classList.remove(`${button}-appear-animation`)
-  }
+  // hoverEffect(button:string){
+  //   let buttonHover = document.getElementById(`${button}`);
+  //   buttonHover!.classList.remove(`${button}-appear-animation`)
+  // }
 }
